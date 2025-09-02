b0a80ce12e88e7fe33f7b3884337ea9d915ce042
JNeubau <neubauer.asia@gmail.com>
2025-09-02 19:42:28 +0200

    Main layout changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import { faImages, faPerson } from '@fortawesome/free-solid-svg-icons'
 import { LAYOUTS, TEXT, DEFAULTS } from './config/parameters'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [layout, setLayout] = useState(DEFAULTS.LAYOUT)
 
   const changeLayout = (newLayout) => {
@@ -18,23 +17,27 @@ function App() {
   return (
     <div className="full-width-container">
       <header className="art-header">
-        <h1>{TEXT.APP_TITLE}</h1>
-        <div style={{alignItems: 'left', display: 'flex', gap: '10px'}}>
-          <p className="subtitle">{TEXT.APP_SUBTITLE}</p>
-          <button 
-            onClick={() => changeLayout(LAYOUTS.ART)}
-            className={`layout-icon ${layout === LAYOUTS.ART ? 'active' : ''}`}
-            title="Art"
-          >
-            <FontAwesomeIcon icon={faImages} size="lg" />
-          </button>
-          <button 
-            onClick={() => changeLayout(LAYOUTS.ABOUT)}
-            className={`layout-icon ${layout === LAYOUTS.ABOUT ? 'active' : ''}`}
-            title="About"
-          >
-            <FontAwesomeIcon icon={faPerson} size="lg" />
-          </button>
+        <div className='header-bar'>
+          <h1 style={{width: '50%', textAlign: 'right'}}>{TEXT.APP_TITLE}</h1>
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'start', width: '50%'}}>
+            <p className="subtitle" style={{marginLeft: '5px'}}>{TEXT.APP_SUBTITLE}</p>
+            <div className='buttons'>
+              <button 
+                onClick={() => changeLayout(LAYOUTS.ART)}
+                className={`layout-icon ${layout === LAYOUTS.ART ? 'active' : ''}`}
+                title="Art"
+              >
+                <FontAwesomeIcon icon={faImages} size="lg" />
+              </button>
+              <button 
+                onClick={() => changeLayout(LAYOUTS.ABOUT)}
+                className={`layout-icon ${layout === LAYOUTS.ABOUT ? 'active' : ''}`}
+                title="About"
+              >
+                <FontAwesomeIcon icon={faPerson} size="lg" />
+              </button>
+            </div>
+          </div>
         </div>
       </header>
 
